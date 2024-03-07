02a72376ac4ed4c12840b8f2673ddd03769aa5b2
rashyad <rashyadarfan@gmail.com>
2024-03-07 12:22:56 +0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,90 @@
-use reqwest::{Client};
-use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::Path;
+use serde_json::{Map, Value};
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Geolocation {
-    pub status: String,
-    pub country: String,
-    pub country_code: String,
-    pub region: String,
-    pub region_name: String,
-    pub city: String,
-    pub lat: f64,
-    pub lon: f64,
-    pub timezone: String,
-    pub isp: String,
-    pub org: String,
+
+fn read_csv_into_json(file_path: &Path){
+
+
+   let contents = fs::read_to_string(file_path)
+   .expect("Should have been able to read the file");
+
+   let cloned_contents = contents.clone();
+
+   let rows: Vec<&str> = cloned_contents.lines().into_iter().collect();
+
+   let row_count = cloned_contents.lines().into_iter().count();
+
+   println!("{:?}", row_count);
+
+   let mut vec = Vec::new();
+
+   let header = rows[0];
+   //let mut map = Map::new();
+
+   for row in rows {
+      //println!("{}", row)
+
+      vec.push(row)
+      
+   }
+   println!("{}", header)
+   // // let collection = parts.collect::<Vec<&str>>();
+
+   // // //map.insert("", Value::String(val));
+   // // for a in collection.into_iter() {
+   // //    println!("{:?}", a)
+   // // }
+
+   // let obj = Value::Object(map);
+
+   //dbg!(vec);
 }
 
-#[tokio::main]
-async fn main() {
+fn read_csv_into_iter(file_path: &Path){
+   
+   let contents = fs::read_to_string(file_path)
+   .expect("Should have been able to read the file");
 
-    // replace the URL with any ip-geolocation vendor url, the one used currently provides free service
-    let ip = "<IP_ADDRESS>";
-    let uri = format!("http://ip-api.com/json/{}", &ip);
-    let result: Geolocation  = Client::new()
-        .get(uri)
-        .send()
-        .await
-        .unwrap()
-        .json()
-        .await
-        .unwrap();
+   let cloned_contents = contents.clone();
 
-    
-    println!("{:#?}", result);
+   let rows: Vec<&str> = cloned_contents.lines().into_iter().collect();
+
+   let row_count = cloned_contents.lines().into_iter().count();
+
+   println!("{:?}", row_count);
+
+   let mut vec = Vec::new();
+
+   let header = rows[0];
+   //let mut map = Map::new();
+
+   for row in rows {
+      //println!("{}", row);
+      let split_row: Vec<&str> = row
+      .split_inclusive(",")
+      .into_iter()
+      .collect();
+
+      vec.push(split_row)
+      
+   }
+   //println!("{}", header)
+   // // let collection = parts.collect::<Vec<&str>>();
+
+   // // //map.insert("", Value::String(val));
+   // // for a in collection.into_iter() {
+   // //    println!("{:?}", a)
+   // // }
+
+   // let obj = Value::Object(map);
+
+   dbg!(vec);
 }
 
+fn main() {
+   let file_path = Path::new("./src/cust100.csv");
+
+   read_csv_into_iter(file_path)
+
+}
